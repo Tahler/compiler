e3571970e235da1d70f00d9804eed099e62d046c
Tyler Berry <tyler@berryac.com>
2017-01-17 23:26:10 -0700

    TokenType includes `fromState`

--- src/edu/neumont/csc330/tokenizer/TokenType.java
+++ src/edu/neumont/csc330/tokenizer/TokenType.java
@@ -47,5 +47,112 @@ public enum TokenType {
 
     IDENTIFIER,
 
-    TRASH_WORD,
+    TRASH_WORD;
+
+    public static TokenType fromState(State endState) {
+        TokenType type;
+        switch (endState) {
+            case DOUBLE:
+                type = TokenType.DOUBLE;
+                break;
+            case ELSE:
+                type = TokenType.ELSE;
+                break;
+            case FOR:
+                type = TokenType.FOR;
+                break;
+            case IF:
+                type = TokenType.IF;
+                break;
+            case INT:
+                type = TokenType.INT;
+                break;
+            case RETURN:
+                type = TokenType.RETURN;
+                break;
+            case STRING:
+                type = TokenType.STRING;
+                break;
+            case VOID:
+                type = TokenType.VOID;
+                break;
+            case WHILE:
+                type = TokenType.WHILE;
+                break;
+            case _EQUALS:
+                type = TokenType.EQUALS;
+                break;
+            case _EQUALS_EQUALS:
+                type = TokenType.EQUALS_EQUALS;
+                break;
+            case _NOT_EQUALS:
+                type = TokenType.NOT_EQUALS;
+                break;
+            case _LESS_THAN:
+                type = TokenType.LESS_THAN;
+                break;
+            case _LESS_THAN_EQUAL:
+                type = TokenType.LESS_THAN_OR_EQUAL;
+                break;
+            case _GREATER_THAN:
+                type = TokenType.GREATER_THAN;
+                break;
+            case _GREATER_THAN_EQUAL:
+                type = TokenType.GREATER_THAN_OR_EQUAL;
+                break;
+            case _OPEN_PARENTHESIS:
+                type = TokenType.OPEN_PARENTHESIS;
+                break;
+            case _CLOSE_PARENTHESIS:
+                type = TokenType.CLOSE_PARENTHESIS;
+                break;
+            case _OPEN_CURLY:
+                type = TokenType.OPEN_CURLY;
+                break;
+            case _CLOSE_CURLY:
+                type = TokenType.CLOSE_CURLY;
+                break;
+            case _OPEN_SQUARE:
+                type = TokenType.OPEN_SQUARE;
+                break;
+            case _CLOSE_SQUARE:
+                type = TokenType.CLOSE_SQUARE;
+                break;
+            case _INT_LITERAL:
+                type = TokenType.INT_LITERAL;
+                break;
+            case _FLOAT_LITERAL:
+                type = TokenType.FLOAT_LITERAL;
+                break;
+            case _COMMA:
+                type = TokenType.COMMA;
+                break;
+            case _SEMICOLON:
+                type = TokenType.SEMICOLON;
+                break;
+            case _PLUS:
+                type = TokenType.PLUS;
+                break;
+            case _MINUS:
+                type = TokenType.MINUS;
+                break;
+            case _ASTERISK:
+                type = TokenType.ASTERISK;
+                break;
+            case _FORWARD_SLASH:
+                type = TokenType.FORWARD_SLASH;
+                break;
+            case _IDENTIFIER:
+                type = TokenType.IDENTIFIER;
+                break;
+            case PRIVATE:
+            case PUBLIC:
+                type = TokenType.TRASH_WORD;
+                break;
+            default:
+                type = null; // TODO
+                break;
+        }
+        return type;
+    }
 }
