d1553f4adf4cfc880883e1af1f4118c8ba6a7f99
Carver Anglin <anglincarver@gmail.com>
2017-01-24 19:39:59 -0700

    start to basic implementation -- we might be missing a lot of states actually...

--- src/edu/neumont/csc330/compiler/parser/Parser.java
+++ src/edu/neumont/csc330/compiler/parser/Parser.java
@@ -7,7 +7,70 @@ import java.util.List;
 import java.util.Stack;
 
 public class Parser {
+    public Node parse(List<Token> tokens) {
+        Stack<Node> stack = new Stack<>();
 
+        for (int ii = 0; ii < tokens.size(); ii++) {
+            Token curr = tokens.get(ii);
+            Token look = (ii < tokens.size()) ? tokens.get(ii + 1) : null;
+
+            stack.push(new TokenNode(curr));
+            reduce(stack, look);
+        }
+
+        if (stack.size() == 1) {
+            Node node = stack.pop();
+            // TODO: verify this is a program node
+            return node;
+        } else {
+            throw new RuntimeException("Tokens could not be reduce to a single node");
+        }
+    }
+
+    public void reduce(Stack<Node> stack, Token look) {
+        Stack<Node> roller = new Stack<>();
+        ReduceState reduceState = ReduceState._INITIAL;
+
+        do {
+            Node node = stack.pop();
+            roller.push(node);
+            reduceState = advanceState(reduceState, look);
+            if (shouldReduce(reduceState, look)) {
+                Node reducedNode = collapse(reduceState, look, stack, roller);
+                stack.push(reducedNode);
+            }
+        } while (reduceState != ReduceState._INVALID && !stack.isEmpty());
+
+        while (!roller.isEmpty()) {
+            Node node = roller.pop();
+            stack.push(node);
+        }
+    }
+
+    private Node collapse(ReduceState reduceState, Token look, Stack<Node> stack, Stack<Node> roller) {
+        return null;
+    }
+
+    private boolean shouldReduce(ReduceState reduceState, Token look) {
+        return false;
+    }
+
+    private ReduceState advanceState(ReduceState reduceState, Token look) {
+        // TODO: everything!
+        return null;
+    }
+}
+
+class Node {
+    List<Node> children;
+}
+
+class TokenNode extends Node {
+    Token value;
+
+    public TokenNode(Token value) {
+        this.value = value;
+    }
 }
 
 enum ReduceState {
